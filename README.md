7036ca3848543f0598c9ad090b75f93405687cbc
sheriffolaoye <sheriffolaoye@gmail.com>
2018-09-18 17:49:10 -0500

    refactored shell_program.c and updated README.md

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ A C program that simulates the linux shell. It simulates basic shell commands. F
 
 # Sample run
 ![screenshot from 2018-09-15 09-57-47](https://user-images.githubusercontent.com/24194821/45587703-f6e86800-b8ce-11e8-970d-fd0e19fab152.png)
+
+# How to Run
+1. run "gcc shell_program.c -o shell"
+2. run "./shell"
+
+# How it works
+* In the main, the getPath function is called to get the system path,
+  the getPath function also parses the path data into a 
+  2D array which is accessible from the main function.
+* A for loop is used to run the shell forever.
+* In the for loop, the user input is received and stored.
+* The user input is checked, if the user enters the "quit" or "exit", the program stops.
+* Otherwise a child process is created.
+* In the child process, the executeCommands function is called to execute the users commands.
+
--- shell_program.c
+++ shell_program.c
@@ -12,66 +12,90 @@
 #include <string.h>
 #include <sys/wait.h>
 
+// function to get the system path
+void getPath(char** paths, int* pathSize);
+// function to execute commands
+void executeCommands(char* input, char** paths, int pathSize);
+
 int main(){
-	char* execPaths = getenv("PATH");	// get all system paths
-
-	char* paths[30] = {NULL};		// create an array and initilize items to NULL
-	char* path = strtok(execPaths, ":");	// split the string
-
-	//printf("System PATH\n");
-	// split execPaths into an array so each path is an array element
-	int i=0;
-	while(path != NULL){
-		paths[i] = path;
-		//printf("%s\n", paths[i]);
-		path = strtok(NULL, ":");
-		i++;
-	}
-	printf("\n");
+	char* paths[30] = {NULL};		// create an array and initilize items to NULL	
+	int numOfPaths;
 	
-	int numOfPaths = i;		// variable to keep track of the number of PATHS available
+	getPath(paths, &numOfPaths);	// call function to get the path
+		
 	char input[50];			// declare char to store user input
 
-	// infinite loop to run the shell forever
+	// infinite loop to run the shell forever until user quits
 	for(;;){
-		printf("Shell >>> ");			// print the shell prompt
-		fgets(input, 50, stdin);		// get user input
+		printf("Shell >>> ");		// print the shell prompt
+		fgets(input, 50, stdin);	// get user input
 
 		// quit program if user executes "quit" or "exit" in our shell
 		if(strcmp(input, "quit\n") == 0 || strcmp(input, "exit\n") == 0){
-			fprintf(stderr, "Exiting shell (o ^ o)\n");
-			return 0;
+			fprintf(stderr, "Exiting shell\n");
+			// exit(0);
+			// return 0;
+			break;
 		}else{	
-			if(fork() == 0){
-				// divide the user input into arguments for the execvp system call
-				const char s[2] = " ";
-			    char* args[20] = {NULL};
-	        	char* x;
-		        i = 0;
-			    x = strtok(input, s);
-		        while(x != NULL){
-	        	        	args[i] = x;
-	                		x = strtok(NULL, s);
-	                		i++;
-				}
-
-				// remove \n from the last string the arguments list
-				args[i - 1][strcspn ( args[i - 1], "\n" )] = '\0';
-				
-				char cmd[255];
-				// iterate through all the paths and run users command using each PATH
-				int k;
-				for(k=0; k < numOfPaths; k++){
-					strcpy(cmd, paths[k]);
-					strcat(cmd, "/");
-					strcat(cmd, args[0]);
-					// remove \n (newline) from the string
-					cmd[strcspn(cmd, "\n" )] = '\0';
-					execvp(cmd, args);
-				}
+			if(fork() == 0){	// create a child process
+				// call method to execute user commands
+				executeCommands(input, paths, numOfPaths);
 			}else{
 				wait(NULL);
 			}
 		}
 	}
+
+	return 0;
+}
+
+// implementation for method to get path directories
+void getPath(char** paths, int* numOfPaths){
+	char* execPaths = getenv("PATH");	// get all system paths
+
+	char* path = strtok(execPaths, ":");	// split the string
+
+	// split execPaths into an array so each path is an array element
+	int i=0;
+	while(path != NULL){
+		paths[i] = path;
+		// printf("%s\n", paths[i]);	// print paths
+		path = strtok(NULL, ":");
+		i++;
+	}
+	printf("\n");
+	
+	*numOfPaths = i;	// variable to keep track of the number of paths available	
+}
+
+// implementation for method to execute commands
+void executeCommands(char* input, char** paths, int pathSize){
+	// divide the user input into arguments for the execvp system call
+	const char s[2] = " ";
+	char* args[20] = {NULL};	// create and initialize array to store arguments to NULL 
+	// declare a C string to store the next extracted argument from the user input string
+	char* x;
+	int i = 0;
+	x = strtok(input, s);
+	while(x != NULL){
+		args[i] = x;
+	        x = strtok(NULL, s);
+	        i++;
+	}
+
+	// remove \n from the last string the arguments list
+	args[i - 1][strcspn ( args[i - 1], "\n" )] = '\0';
+	
+	char cmd[255];	// declare a char to store the user
+	int k;		// declare an int to for the for loop
+	// iterate through all the paths and run users command using each PATH
+	for(k=0; k < pathSize; k++){
+		strcpy(cmd, paths[k]);	// copy the path into cmd
+		strcat(cmd, "/");	// add a forward slash to the string cmd
+		strcat(cmd, args[0]);	// add the first command user enters to the string cmd
+		cmd[strcspn(cmd, "\n" )] = '\0';	// remove \n from the string
+		execvp(cmd, args);
+	}
+
+	fprintf(stderr, "No such command\n");	// print error message if the command doesnt exist
 }
